9fa39726173eec3f816c4bcff4d38ca0f271de9b
Byan <byanjati@gmail.com>
2015-08-31 23:20:42 +0700

    Flatmap
    Nested Flatmap

--- sicpex/src/sicpex/SICP24.clj
+++ sicpex/src/sicpex/SICP24.clj
@@ -14,6 +14,14 @@
 ;; Filter -> (Permutation)
 ;;           -> (FlatMap Lambda (map lambda seq) seq)
 
-
 (defn find-triplet [n s]
-  )
+  (filter #(not (or (= % nil) (empty? %)))
+    (flatmap
+     (fn [k]
+       (flatmap
+        (fn [j]
+          (map (fn [i] (list i j k))(range 1 j)))
+        (range 1 k)))
+     (range 1 (+ n s)))))
+
+(flatmap (fn [i] (map (fn [j] (list i j)) (range 1 i))) (range 1 5))
